189bb7dda0435d3937f8a37d28856af520f23430
Venki099 <venkatesh.s@datalyzer.com>
2025-05-31 23:12:53 +0530

    Cicd Build

--- Score.py
+++ Score.py
@@ -1,12 +1,26 @@
 from turtle import Turtle
+import os
+import sys
+# Add this helper function at the top of the file
+def resource_path(filename):
+    try:
+        base_path = sys._MEIPASS  # PyInstaller temp directory
+    except AttributeError:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, filename)
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
+        self.file_path = resource_path("new_file")
         # self.highscore = 0
-        with open("new_file",mode="r") as newr:
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, "w") as f:
+                f.write("0")
+
+        with open(self.file_path, mode="r") as newr:
             content = newr.read()
-            self.highscore = int(content)
+            self.highscore = int(content) if content.strip().isdigit() else 0
             # if content != "":
             #     self.highscore = int(content)
             # else:
@@ -38,7 +52,7 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
-            with open("new_file",mode="w") as new:
+            with open(self.file_path,mode="w") as new:
                 new.write(str(self.highscore))
         self.score = 0
         self.update_score()
